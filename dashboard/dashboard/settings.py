0583557bec0c7203df87e3430a1c832ed91e93f7
Yoonjae Park <yj0604.park@gmail.com>
2019-08-11 23:13:05 +0900

    settings

--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = open("dashboard/secret.txt").read().strip()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["paryoja.asuscomm.com"]
 
 
 # Application definition
--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-docker run --rm -it -v `pwd`:/work yj0604park/dashboard:latest /bin/bash
+docker run --rm -it -p 8000:8000 -v `pwd`:/work yj0604park/dashboard:latest /bin/bash
