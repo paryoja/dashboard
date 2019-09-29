881cf623b39f8620c9e20f9075e65e9dd29d83a2
Yoonjae Park <paryoja@hanmail.net>
2019-09-29 15:56:51 +0900

    default settings

--- dashboard/dashboard/urls.py
+++ dashboard/dashboard/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-    path('bstest/', include('bstest.urls')),
+    path('timezone/', include('timezone.urls')),
     path('admin/', admin.site.urls),
 ]
