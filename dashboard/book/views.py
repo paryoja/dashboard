f4e747addc0a650535c5ebe9c90d40859b107c10
Yoonjae Park <paryoja@hanmail.net>
2019-10-14 00:38:59 +0900

    refactoring xml parser

--- dashboard/book/views.py
+++ dashboard/book/views.py
@@ -1,7 +1,9 @@
-import xml.etree.ElementTree as ET
+from xml.etree import cElementTree as ElementTree
 
 import requests
 from django.shortcuts import render
+
+from .xml_helper import XmlDictConfig
 
 
 def get_render_dict(current_page):
@@ -33,73 +35,6 @@ def wine(request):
     return render(request, 'book/wine.html', render_dict)
 
 
-from xml.etree import cElementTree as ElementTree
-
-
-class XmlListConfig(list):
-    def __init__(self, aList):
-        for element in aList:
-            if element:
-                # treat like dict
-                if len(element) == 1 or element[0].tag != element[1].tag:
-                    self.append(XmlDictConfig(element))
-                # treat like list
-                elif element[0].tag == element[1].tag:
-                    self.append(XmlListConfig(element))
-            elif element.text:
-                text = element.text.strip()
-                if text:
-                    self.append(text)
-
-
-class XmlDictConfig(dict):
-    '''
-    Example usage:
-
-    >>> tree = ElementTree.parse('your_file.xml')
-    >>> root = tree.getroot()
-    >>> xmldict = XmlDictConfig(root)
-
-    Or, if you want to use an XML string:
-
-    >>> root = ElementTree.XML(xml_string)
-    >>> xmldict = XmlDictConfig(root)
-
-    And then use xmldict for what it is... a dict.
-    '''
-
-    def __init__(self, parent_element):
-        if parent_element.items():
-            self.update(dict(parent_element.items()))
-        for element in parent_element:
-            if element:
-                # treat like dict - we assume that if the first two tags
-                # in a series are different, then they are all different.
-                if len(element) == 1 or element[0].tag != element[1].tag:
-                    aDict = XmlDictConfig(element)
-                # treat like list - we assume that if the first two tags
-                # in a series are the same, then the rest are the same.
-                else:
-                    # here, we put the list in dictionary; the key is the
-                    # tag name the list elements all share in common, and
-                    # the value is the list itself
-                    aDict = {element[0].tag: XmlListConfig(element)}
-                # if the tag has attributes, add those to the dict
-                if element.items():
-                    aDict.update(dict(element.items()))
-                self.update({element.tag: aDict})
-            # this assumes that if you've got an attribute in a tag,
-            # you won't be having any text. This may or may not be a
-            # good idea -- time will tell. It works for the way we are
-            # currently doing XML configuration files...
-            elif element.items():
-                self.update({element.tag: dict(element.items())})
-            # finally, if there are no child tags and no attributes, extract
-            # the text
-            else:
-                self.update({element.tag: element.text})
-
-
 query_url = "http://www.law.go.kr/DRF/lawSearch.do?OC=test&target=law&type=XML&mobileYn=Y&query="
 
 
@@ -116,7 +51,7 @@ def law_search(request):
         result = requests.post(url).text
         render_dict['result'] = result
 
-        tree = ET.ElementTree(ET.fromstring(result))
+        tree = ElementTree.ElementTree(ElementTree.fromstring(result))
         law_list = [XmlDictConfig(node) for node in tree.findall('law')]
         render_dict['law_list'] = law_list
 
