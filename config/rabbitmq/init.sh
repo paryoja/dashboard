8758195dc0f717e30dae44aed2dcfd4d062efbb5
Yoonjae Park <paryoja@hanmail.net>
2020-01-05 20:14:48 +0900

    add initialize scripts

---
+++ config/rabbitmq/init.sh
@@ -0,0 +1,4 @@
+rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
+rabbitmqctl set_user_tags $RABBITMQ_USER administrator ; \
+rabbitmqctl set_permissions -p / $RABBITMQ_USER  ".*" ".*" ".*" ; \
+rabbitmqctl add_vhost myvhost
--- dashboard/dashboard/settings.py
+++ dashboard/dashboard/settings.py
@@ -135,6 +135,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = './.static_root'
 
 LOGGING = {
     'version': 1,
