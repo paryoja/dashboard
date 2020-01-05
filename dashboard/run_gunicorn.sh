11c97d77bfc3504cf14e1a4869cb62729767de13
Yoonjae Park <paryoja@hanmail.net>
2020-01-05 18:34:11 +0900

    modify compose

--- dashboard/run_gunicorn.sh
+++ dashboard/run_gunicorn.sh
@@ -3,5 +3,6 @@ export PYTHONIOENCODING=utf-8
 
 python3 manage.py makemigrations
 python3 manage.py migrate
+echo yes | python3 manage.py collectstatic
 
 gunicorn dashboard.wsgi:application --bind 0.0.0.0:8000
