4eda0d2acc00e6f584139a8963d51e2d21191d17
Yoonjae Park <paryoja@hanmail.net>
2020-01-05 21:56:50 +0900

    modify rabbitmq docker file

--- config/rabbitmq/init.sh
+++ config/rabbitmq/init.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 (rabbitmqctl wait /var/lib/rabbitmq/mnesia/rabbitmq.pid; \
-rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD; \
-rabbitmqctl set_user_tags $RABBITMQ_USER administrator; \
+rabbitmqctl add_user myuser mypassword; \
 rabbitmqctl add_vhost myvhost; \
-rabbitmqctl set_permissions -p myvhost $RABBITMQ_USER  ".*" ".*" ".*";) &
+rabbitmqctl set_user_tags myuser mytag; \
+rabbitmqctl set_permissions -p myvhost myuser ".*" ".*" ".*";) &
 
 # $@ is used to pass arguments to the rabbitmq-server command.
 # For example if you use it like this: docker run -d rabbitmq arg1 arg2,
