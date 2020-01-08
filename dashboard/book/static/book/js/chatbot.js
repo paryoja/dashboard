e8df866a775ba93f587e57417cd5e3debfe66037
Yoonjae Park <paryoja@hanmail.net>
2020-01-09 00:07:15 +0900

    add enter behavior

--- dashboard/book/static/book/js/chatbot.js
+++ dashboard/book/static/book/js/chatbot.js
@@ -1,13 +1,16 @@
-$("button").click(function () {
-    var message = $("#message").val()
-    var csrf_token = $("input[type='hidden']").val()
+$("#message_form").submit(function (e) {
+    e.preventDefault();
+    var message = $("#message").val();
+    var csrf_token = $("input[type='hidden']").val();
+
     $.ajax({
         type: "POST",
         url: "/book/query_chatbot/",
         data: {'message': message, 'csrfmiddlewaretoken': csrf_token},
         dataType: "json",
         success: function (response) { // 통신 성공시 - 동적으로 좋아요 갯수 변경, 유저 목록 변경
-            let message_area = $("#message_area")
+
+            let message_area = $("#message_area");
             message_area.append("\nYou: " + message + "\nRasa: " + response[0].text);
             message_area.css('height', message_area.height() + 62);
         },
